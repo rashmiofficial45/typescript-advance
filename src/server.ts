926d8bfebb03f149bb7e166790d8fccf4c6b5461
Rashmi Ranjan Parija <rashmiranjanparija78898@gmail.com>
2025-02-13 23:15:10 +0530

    Add User interface and updateUser function with Pick utility type

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+//Note: Common use of Interface
 interface User{
     name: string,
     age :number
@@ -10,4 +11,21 @@ function UserInfo (user:User){
 
 const user = UserInfo({name:"rashmi",age:32})
 console.log(user);
+
+//Pick Property
+
+interface User {
+  id:string;
+  name: string;
+  age: number;
+  email:string;
+  password:string;
+}
+
+type updatedUserProps = Pick<User, "id" | "name" | "age">
+
+function updateUser(updatedUser: updatedUserProps) {
+  console.log(user.age, user.name);
+  return user;
+}
 
