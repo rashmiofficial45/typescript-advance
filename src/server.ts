709bc287c54e458e77c3b10e449eddc8d066650e
Rashmi Ranjan Parija <rashmiranjanparija78898@gmail.com>
2025-02-15 20:53:52 +0530

    Refactor function names to improve clarity and prevent misuse of return types in TypeScript

--- src/server.ts
+++ src/server.ts
@@ -92,18 +92,18 @@ function reverse(s: string): string;
 // ❌ Don’t use the return type any for callbacks whose value will be ignored:
 
 /* WRONG */
-function fn(x: () => any) {
+function fn1(x: () => any) {
   x();
 }
 // ✅ Do use the return type void for callbacks whose value will be ignored:
 
 /* OK */
-function fn(x: () => void) {
+function fn2(x: () => void) {
   x();
 }
 // Why: Using void is safer because it prevents you from accidentally using the return value of x in an unchecked way:
 
-function fn(x: () => void) {
+function fn3(x: () => void) {
   var k = x(); // oops! meant to do something else
   k.doSomething(); // error, but would be OK if the return type had been 'any'
 }
